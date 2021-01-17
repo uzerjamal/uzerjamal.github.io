9f3ae6ced8e7e806c4663f4e43b6dce84a57d56b
Uzer Jamal <uzerjamal@gmail.com>
2021-01-17 19:54:41 +0530

    changed nav to use height instead of padding, made links smaller

--- src/nav.js
+++ src/nav.js
@@ -3,12 +3,12 @@ import './style/tailwind.css';
 function Nav(){
     return(
         <div>
-            <header class="min-w-full px-8 py-4 bg-black flex justify-between">
+            <header class="min-w-full px-8 h-16 bg-black flex justify-between items-center">
                 <div>
-                    <span class='text-2xl font-extrabold text-white hover:text-pink transition'>UZER.</span>
+                    <span class='text-2xl font-extrabold text-white hover:text-pink transition bg-pink px-2 hover:bg-white'>UZER.</span>
                 </div>
                 <div>
-                    <nav class="text-2xl text-white font-light">
+                    <nav class="text-lg text-white font-light">
                         <a href="#" class="hover:text-pink transition px-2">ABOUT</a>
                         <a href="#" class="hover:text-pink transition px-2">SKILLS</a>
                         <a href="#" class="hover:text-pink transition px-2">PROJECTS</a>
